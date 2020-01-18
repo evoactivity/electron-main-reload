24ffa2f565688f25b6e6ab2f716cda7a0312d033
Liam Potter <radioactivity@gmail.com>
2020-01-18 08:34:16 +0000

    only do main reloads
    ember handles browser window

--- main.js
+++ main.js
@@ -41,42 +41,10 @@ const createHardresetHandler = (eXecutable, hardResetMethod, argv) =>
   }
 
 module.exports = (glob, options = {}) => {
-  const browserWindows = []
-  const watcher = chokidar.watch(glob, Object.assign({ ignored: [ignoredPaths, mainFile] }, options))
-
-  // Callback function to be executed:
-  // I) soft reset: reload browser windows
-  const softResetHandler = () => browserWindows.forEach(bw => bw.webContents.reloadIgnoringCache())
-  // II) hard reset: restart the whole electron process
-  const eXecutable = options.electron
-  const hardResetHandler = createHardresetHandler(eXecutable, options.hardResetMethod, options.argv)
-
-  // Add each created BrowserWindow to list of maintained items
-  app.on('browser-window-created', (e, bw) => {
-    browserWindows.push(bw)
-
-    // Remove closed windows from list of maintained items
-    bw.on('closed', function () {
-      const i = browserWindows.indexOf(bw) // Must use current index
-      browserWindows.splice(i, 1)
-    })
-  })
-
-  // Enable default soft reset
-  watcher.on('change', softResetHandler)
-
   // Preparing hard reset if electron executable is given in options
   // A hard reset is only done when the main file has changed
   if (eXecutable && fs.existsSync(eXecutable)) {
     const hardWatcher = chokidar.watch(mainFile, Object.assign({ ignored: [ignoredPaths] }, options))
-
-    if (options.forceHardReset === true) {
-      // Watch every file for hard reset and not only the main file
-      hardWatcher.add(glob)
-      // Stop our default soft reset
-      watcher.close()
-    }
-
     hardWatcher.once('change', hardResetHandler)
   } else {
     console.log('Electron could not be found. No hard resets for you!')
